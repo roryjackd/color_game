fe854da60ceb9e0497356df79fbc1050da5cd730
roryjackd <roryjackd@yahoo.com>
2017-05-26 14:51:38 -0600

    random color pick

--- colorGame.js
+++ colorGame.js
@@ -1,29 +1,44 @@
 var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
+    'rgb(255, 0, 0)',
+    'rgb(255, 255, 0)',
+    'rgb(0, 255, 0)',
+    'rgb(0, 255, 255)',
+    'rgb(0, 0, 255)',
+    'rgb(255, 0, 255)'
 ]
 
-var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
-var colorDisplay = document.getElementById("colorDisplay");
+var squares = document.querySelectorAll('.square');
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById('colorDisplay');
+var messageDisplay = document.querySelector('#message');
 
 colorDisplay.textContent = pickedColor;
 
 for(var i=0; i < squares.length; i++){
     squares[i].style.background = colors[i];
 
-    squares[i].addEventListener("click", function(){
+    squares[i].addEventListener('click', function(){
         var clickedColor = this.style.background;
 
         if(clickedColor === pickedColor){
-            alert("Correct!");
+            messageDisplay.textContent = 'Correct!';
+            changeColors(clickedColor);
         } else {
-            alert("WRONG!!!");
+            this.style.background = '#232323';
+            messageDisplay.textContent = 'Try Again'
         }
     });
 }
 
+function changeColors(color){
+
+    for(var i = 0; i < squares.length; i++){
+        squares[i].style.background = color;
+    }
+
+}
+
+function pickColor(){
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
