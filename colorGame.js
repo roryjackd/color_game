8ecb6e1dc05ae3d2ab34476df05033c50b8183c0
roryjackd <roryjackd@yahoo.com>
2017-05-28 13:39:13 -0600

    randomColor()

--- colorGame.js
+++ colorGame.js
@@ -7,10 +7,12 @@ var colors = [
     'rgb(255, 0, 255)'
 ]
 
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll('.square');
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
 var messageDisplay = document.querySelector('#message');
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -20,9 +22,12 @@ for(var i=0; i < squares.length; i++){
     squares[i].addEventListener('click', function(){
         var clickedColor = this.style.background;
 
+        console.log(clickedColor, pickedColor)
+
         if(clickedColor === pickedColor){
             messageDisplay.textContent = 'Correct!';
             changeColors(clickedColor);
+            h1.style.background = clickedColor;
         } else {
             this.style.background = '#232323';
             messageDisplay.textContent = 'Try Again'
@@ -42,3 +47,21 @@ function pickColor(){
     var random = Math.floor(Math.random() * colors.length);
     return colors[random];
 }
+
+function generateRandomColors(num){
+    var arr = []
+
+    for(var i = 0; i < num; i++){
+        arr.push(randomColor())
+    }
+
+    return arr;
+}
+
+function randomColor(){
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+}
+
